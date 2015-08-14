ead5ceb0770e47b8e7ecdd9f6f486f67ac96e5b5
unknown <stevengrimes83@gmail.com>
2015-08-14 13:05:06 -0700

    made corrections to problems

--- finalReview/Problem2/problem2.js
+++ finalReview/Problem2/problem2.js
@@ -6,20 +6,20 @@ jQuery has been imported for you.
 A client has contacted you because they've written terrible JavaScript.
 Repair their code! There are 10 bugs in the code. Make a short description of what you've fixed.
 
-1.  Added () after function
-2.  added . between .ul and li
-3.  added '' around square
-4.  ?
-5.  ?
-6.  ?
-7.  ?
-8.  ?
-9.  ?
-10. ?
+1.  Added $ before document
+2.  Added () after function
+3.  added . between .ul and li
+4.  added '' around square
+5.  removed 'nav'
+6.  added () after .text
+7.  deleted . before p
+8.  
+9.  
+10. 
 */
 
-document.ready(function() {
-    $('nav .ul.li').css('list-style-type','square');
+$document.ready(function() {
+    $('.ul.li').css('list-style-type','square');
 
-    $('.output').text('.p'.text().split( ).length());
+    $('.output').text('p'.text().split( ).length());
 });
